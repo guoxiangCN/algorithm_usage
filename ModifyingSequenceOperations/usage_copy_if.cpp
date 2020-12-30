f6e185b7f8278ef7a121dfcf76b46660e4a0778a
guoxiang2 <guoxiang2@yy.com>
2020-12-30 18:08:53 +0800

    auto-commit

--- ModifyingSequenceOperations/usage_copy_if.cpp
+++ ModifyingSequenceOperations/usage_copy_if.cpp
@@ -1,3 +1,4 @@
+#include "../my_algorithm.h"
 #include <algorithm>
 #include <array>
 #include <iostream>
@@ -5,19 +6,24 @@
 #include <vector>
 
 int main(int argc, char **argv) {
+
   //
   std::array<int, 10> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
   // std::vector<int> v; core dump
   std::vector<int> v(arr.size());
 
-  auto outIter = std::copy_if(arr.begin(), arr.end(), v.begin(),
-                              [](const int &x) { return x % 2 != 0; });
+  // auto outIter = std::copy_if(arr.begin(), arr.end(), v.begin(), [](const int
+  // &x) { return x % 2 != 0; });
 
+  auto outIter = my::copy_if(arr.begin(), arr.end(), v.begin(),
+                             [](const int &x) { return x % 2 != 0; });
   v.resize(std::distance(v.begin(), outIter));
 
   for (const auto &x : v) {
     std::cout << x << ",";
   }
+
+  v.resize(0);
   return 0;
 }
--- my_algorithm.h
+++ my_algorithm.h
@@ -1,11 +1,31 @@
 #ifndef __MY_ALGORITHM_LIKE_STL__
 #define __MY_ALGORITHM_LIKE_STL__
 
+namespace my {
 // std::min
 // std::max
 // std::minmax
 // std::min_element
 // std::max_element
 // std::minmax_element
+//
 
+// std::copy
+// std::copy_if
+// std::copy_n
+
+template <typename InputIterator, typename OutputIterator, typename _Pred>
+OutputIterator copy_if(InputIterator first, InputIterator last,
+                       OutputIterator output, _Pred pred) {
+  while (first != last) {
+    if (pred(*first)) {
+      *output = *first;
+      output++;
+    }
+    first++;
+  }
+  return output;
+}
+
+} // namespace my
 #endif
