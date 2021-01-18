4f434662f40135a3655a396fde2eeb516d62f10d
root <guoxaing>
2021-01-18 08:29:56 +0800

    a

--- sort_heap.h
+++ sort_heap.h
@@ -9,9 +9,9 @@
  * adjustPos 需要调整的数组的索引位置
  */
 template <typename T, typename Compare>
-void adjustHeap(std::vector<T> &arr, Compare compare, int adjustPos)
+void adjustHeap(std::vector<T> &arr, Compare compare, int limit, int adjustPos)
 {
-    int maxPos = arr.size() - 1;
+    int maxPos = limit - 1;
     int toBeParentPos = adjustPos;
 
     int leftPos = 2 * adjustPos + 1;
@@ -25,7 +25,7 @@ void adjustHeap(std::vector<T> &arr, Compare compare, int adjustPos)
     if (toBeParentPos != adjustPos)
     {
         std::swap(arr[toBeParentPos], arr[adjustPos]);
-        adjustHeap(arr, compare, toBeParentPos);
+        adjustHeap(arr, compare, limit, toBeParentPos);
     }
 }
 
@@ -35,8 +35,13 @@ void heapSort(std::vector<T> &arr, Compare compare)
     // 无顺序数组变成堆, 需要从最后的节点往堆顶维护 O(N)
     for (int i = (arr.size() / 2) - 1  ; i >= 0; i--)
     {
-        adjustHeap(arr, compare, i);
+        adjustHeap(arr, compare,arr.size(), i);
     }
     
     // 排序
+    for (int i = arr.size() - 1; i > 0; i--)  {
+        std::swap(arr[0], arr[i]);
+        adjustHeap(arr,compare, i, 0);
+    }
+    
 }
